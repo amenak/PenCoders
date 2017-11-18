46cd7c9e98d22359d75560b4da0862fdad59a487
Elie Elisee Paul <marcoht023@hotmail.com>
2017-11-18 09:05:29 -0500

    saved home.js last changes

--- penBook-app/controllers/home.js
+++ penBook-app/controllers/home.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('../middlewares/authentication');
 const redirect = require('../middlewares/redirect');
-const getSlug = require('speakingurl');
 
 const router = express.Router();
 
